2700e8f4ca1820b01429976866768b769252b206
wattanx <wattan.dev@gmail.com>
2022-04-30 22:21:33 +0900

    feat: replace BreadCrumb

--- components/BreadCrumb.tsx
+++ components/BreadCrumb.tsx
@@ -1,10 +1,27 @@
-import Link from 'next/link';
+import NextLink from 'next/link';
+import { List, ListItem } from '@chakra-ui/layout';
+import { chakra } from '@chakra-ui/system';
 import { ICategory, ITag } from '@/types';
 
 type BreadCrumbProps = {
   category?: ICategory;
   tag?: ITag;
 };
+
+const BreadCrumbList: React.FC<{ children: React.ReactNode; href: string }> = ({
+  children,
+  href,
+}) => (
+  <ListItem
+    color="#616269"
+    _after={{ content: '">"', my: 0, mx: '10px' }}
+    _last={{ _after: { content: '""', margin: 0 } }}
+  >
+    <NextLink href={href} passHref prefetch={false}>
+      <chakra.a color="#331cbf">{children}</chakra.a>
+    </NextLink>
+  </ListItem>
+);
 
 export const BreadCrumb: React.FC<BreadCrumbProps> = (props) => {
   const hasCategory = (category?: ICategory) => {
@@ -22,20 +39,16 @@ export const BreadCrumb: React.FC<BreadCrumbProps> = (props) => {
   };
 
   return (
-    <ul className="breadcrumb">
-      <li className="breadcrumbList">
-        <Link href="/">記事一覧</Link>
-      </li>
+    <List display="flex" flexWrap="wrap" pt="20px">
+      <BreadCrumbList href="/">記事一覧</BreadCrumbList>
       {hasCategory(props.category) && (
-        <li className="breadcrumbList">
-          <Link href={`/category/${props.category?.id}/page/1`}>{props.category?.name}</Link>
-        </li>
+        <BreadCrumbList href={`/category/${props.category?.id}/page/1`}>
+          {props.category?.name}
+        </BreadCrumbList>
       )}
       {hasTag(props.tag) && (
-        <li className="breadcrumbList">
-          <Link href={`/tag/${props.tag?.id}/page/1`}>{props.tag?.name}</Link>
-        </li>
+        <BreadCrumbList href={`/tag/${props.tag?.id}/page/1`}>{props.tag?.name}</BreadCrumbList>
       )}
-    </ul>
+    </List>
   );
 };
