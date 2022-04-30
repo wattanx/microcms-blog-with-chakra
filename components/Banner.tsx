818b2bffdd8f3afbf5c971fdcab86413c8a77b02
wattanx <wattan.dev@gmail.com>
2022-04-30 21:46:48 +0900

    feat: replace banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,32 +1,35 @@
+import { Box } from '@chakra-ui/layout';
+import { chakra } from '@chakra-ui/system';
 import { IBanner } from '@/types';
-import styles from '@styles/components/Banner.module.scss';
 
 type BannerProps = {
   banner: IBanner;
 };
 
-export const Banner: React.FC<BannerProps> = (props) => {
+export const Banner: React.FC<BannerProps> = ({ banner }) => {
   return (
-    <div className="wrapper">
-      <a
+    <Box textAlign={{ xl: 'center' }}>
+      <chakra.a
         href={
-          props.banner?.url
-            ? `${props.banner.url}?utm_source=google&utm_medium=content-text&utm_campain=remarketing`
+          banner?.url
+            ? `${banner.url}?utm_source=google&utm_medium=content-text&utm_campain=remarketing`
             : ''
         }
       >
-        <picture>
-          <source
+        <chakra.picture>
+          <chakra.source
             type="image/webp"
-            srcSet={`${props.banner.image.url}?w=300&fm=webp,${props.banner.image.url}?w=600&fm=webp 2x`}
+            srcSet={`${banner.image.url}?w=300&fm=webp,${banner.image.url}?w=600&fm=webp 2x`}
           />
-          <img
-            src={props.banner?.image?.url}
-            className={`${styles.image} lazyload`}
-            alt={props.banner?.alt}
+          <chakra.img
+            width="300px"
+            borderRadius="5px"
+            src={banner?.image?.url}
+            className={`lazyload`}
+            alt={banner?.alt}
           />
-        </picture>
-      </a>
-    </div>
+        </chakra.picture>
+      </chakra.a>
+    </Box>
   );
 };
